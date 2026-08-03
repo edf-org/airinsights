9059fae8b5adeabd8dd75986e6568e689f252183
dan-peters <dpeters@edf.org>
2026-08-03 13:50:42 -0500

    modify helpers to catch timezone formatting errors

--- airinsights/helpers.py
+++ airinsights/helpers.py
@@ -156,14 +156,16 @@ def read_aqdata_file(
     # --- Take the file extension to determine which pandas function to use
     file_ext = Path(data_path).suffix.lower()
     if file_ext == '.csv':
-        df = pd.read_csv(input_file, parse_dates=[config_dict['timestamp_col']], date_format=config_dict['timestamp_format']) 
+        df = pd.read_csv(input_file) 
     elif file_ext in ('.xls', '.xlsx', '.xlsm'):
-        df = pd.read_excel(input_file, parse_dates=[config_dict['timestamp_col']], date_format=config_dict['timestamp_format'])
+        df = pd.read_excel(input_file)
     elif file_ext == '.json':
         df = pd.read_json(input_file)
-        df[config_dict['timestamp_col']] = pd.to_datetime(df[config_dict['timestamp_col']], format=config_dict['timestamp_format'])
     else:
         raise ValueError(f"Unsupported file format: {file_ext}. Supported file formats are csv, json, and excel files (xsl, xlsx, xlsm)")
+    
+    # --- Format date column using config. This will throw error if it fails
+    df[config_dict['timestamp_col']] = pd.to_datetime(df[config_dict['timestamp_col']],format=config_dict['timestamp_format'])
     
     df = melt_long(df,config_dict)
     df = localize_tz(df)
